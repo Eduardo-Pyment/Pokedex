6489d7134c5f69b5a84f327b62de8db47ed66038
Eduardo-Pyment <edupymota@protonmail.com>
2023-12-12 17:19:17 -0300

    Handling da pokeAPI separado do script main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
     <span class="number">001</span>
@@ -19,15 +15,11 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            console.log(convertPokemonToLi(pokemon))
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
+pokeAPI.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        console.log(convertPokemonToLi(pokemon))
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+})
     .catch((error) => console.log(error))
